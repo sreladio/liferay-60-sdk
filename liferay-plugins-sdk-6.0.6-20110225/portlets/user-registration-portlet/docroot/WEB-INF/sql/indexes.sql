65cf00979f30e13121ba9a741cbd417edc00af92
eladio <sr.eladio@gmail.com>
2013-02-24 12:19:08 +0100

    Borrado user-registration-portlet

--- liferay-plugins-sdk-6.0.6-20110225/portlets/user-registration-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,7 +0,0 @@
-create index IX_8FEAE321 on ME_member (companyId);
-create index IX_1363BDE3 on ME_member (groupId);
-create index IX_D8C00192 on ME_member (groupId, memberLastName);
-
-create index IX_F89835E9 on US_USMember (companyId);
-create index IX_AB5722AB on US_USMember (groupId);
-create index IX_8A20D65A on US_USMember (groupId, memberLastName);
